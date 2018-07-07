44058a031741f3110a3b28b427eff2bd9fd73caf
Edward <edpantzar@gmail.com>
2018-07-07 15:40:55 -0500

    Added production db url

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,11 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/new-here'
+  },
+
+  production: {
+    client: 'pg',
+    connection: 'postgres://dkfrqwvaozwfpq:2d2ac080649ec4c4347bcb3bb38fd55d10953770d9ce666045e9bddf0ff6814b@ec2-54-235-196-250.compute-1.amazonaws.com:5432/d25lo6hujsck2d'
   }
 
 };
