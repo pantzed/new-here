e938417c2c052b6eb2d4ef506ccbd93e6623b9a0
Edward <edpantzar@gmail.com>
2018-07-08 12:57:36 -0500

    Added signin post route

--- routes/signin.js
+++ routes/signin.js
@@ -9,26 +9,28 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../knexfile')[env];
 const knex = require('knex')(config);
 const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client(CLIENT_ID);
+const client = new OAuth2Client("1074020877158-eb30cjftrhcvulod09gqgtbnbjqj7srg.apps.googleusercontent.com");
+
+
+router.get('/signin', (req, res) => {
+  res.render('signin', {title: 'SignIn Page'});
+});
 
 router.post('/signin/:id', (req, res) => {
-  let token = req.body.id;
-  // async function verify() {
-  //   const ticket = await client.verifyIdToken({
-  //       idToken: token,
-  //       audience: "1074020877158-eb30cjftrhcvulod09gqgtbnbjqj7srg.apps.googleusercontent.com"  // Specify the CLIENT_ID of the app that accesses the backend
-  //       // Or, if multiple clients access the backend:
-  //       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  //   });
-  //   const payload = ticket.getPayload();
-  //   const userid = payload['sub'];
-  //   // If request specified a G Suite domain:
-  //   //const domain = payload['hd'];
-  // }
-  // verify().catch(console.error);
-  // console.log('fjhlajksdhfjadhf');
-  console.log(token);
-  res.render('profile', {title: 'Profile Page'});
+  async function verify() {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+  }
+  verify().catch(console.error);
+  res.send(`${req.params.id}`);
 });
 
 module.exports = router;
