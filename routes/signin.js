17373df13e2de66c2f887d297ab1dfda126f2c4e
Edward <edpantzar@gmail.com>
2018-07-08 12:28:50 -0500

    debuging

--- routes/signin.js
+++ routes/signin.js
@@ -13,20 +13,21 @@ const client = new OAuth2Client(CLIENT_ID);
 
 router.post('/signin/:id', (req, res) => {
   let token = req.body.id;
-  async function verify() {
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: "1074020877158-eb30cjftrhcvulod09gqgtbnbjqj7srg.apps.googleusercontent.com"  // Specify the CLIENT_ID of the app that accesses the backend
-        // Or, if multiple clients access the backend:
-        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-    });
-    const payload = ticket.getPayload();
-    const userid = payload['sub'];
-    // If request specified a G Suite domain:
-    //const domain = payload['hd'];
-  }
-  verify().catch(console.error);
-  console.log('fjhlajksdhfjadhf');
+  // async function verify() {
+  //   const ticket = await client.verifyIdToken({
+  //       idToken: token,
+  //       audience: "1074020877158-eb30cjftrhcvulod09gqgtbnbjqj7srg.apps.googleusercontent.com"  // Specify the CLIENT_ID of the app that accesses the backend
+  //       // Or, if multiple clients access the backend:
+  //       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  //   });
+  //   const payload = ticket.getPayload();
+  //   const userid = payload['sub'];
+  //   // If request specified a G Suite domain:
+  //   //const domain = payload['hd'];
+  // }
+  // verify().catch(console.error);
+  // console.log('fjhlajksdhfjadhf');
+  console.log(token);
   res.render('profile', {title: 'Profile Page'});
 });
 
