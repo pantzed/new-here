c4b74694cfaa041c68ab493d5311674c6ac703f0
Edward <edpantzar@gmail.com>
2018-07-08 12:32:13 -0500

    Modified google functions

--- public/javascripts/google.js
+++ public/javascripts/google.js
@@ -2,7 +2,7 @@
 
   function onSignIn(googleUser) {
     let id_token = googleUser.getAuthResponse().id_token;
-    fetch(`/signin/${id_token}`, {method: 'POST'});
+    postUser(id_token);
   }
 
   function signOut() {
@@ -10,6 +10,10 @@
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
+  }
+
+  function postUser(id_token) {
+    fetch(`/signin/${id_token}`, {method: 'POST'});
   }
 
 })();
