1dc46d01a4fb8b57d73e18b8e602c4bfbe224fa1
Edward <edpantzar@gmail.com>
2018-07-08 11:42:53 -0500

    Added event listener for signout

--- public/javascripts/google-tools.js
+++ public/javascripts/google-tools.js
@@ -1,5 +1,7 @@
 (function () {
-  
+
+  document.getElementById('google-signout').addEventListener('click', signOut);
+
    function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
