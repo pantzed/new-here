ddb8ea9c4a8bc7434cc42c47f572c1fdd7de3e67
Edward <edpantzar@gmail.com>
2018-07-09 16:15:12 -0500

    Made create event form functional

--- routes/events.js
+++ routes/events.js
@@ -9,6 +9,19 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../knexfile')[env];
 const knex = require('knex')(config);
 
+
+router.get('/events/add', (req, res) => {
+  res.render('create_event', {title: "Create an Event"});
+});
+
+router.get('/events/:id/edit', (req, res) => {
+  res.render('edit_event', {title: 'Edit Event'});
+});
+
+router.get('/events/:id', (req, res) => {
+  res.render('event', {title: 'This Event'});
+})
+
 router.get('/events', (req, res) => {
   knex('events')
   .then((events) => {
@@ -16,16 +29,21 @@ router.get('/events', (req, res) => {
   })
 });
 
-router.get('/events/:id', (req, res) => {
-  res.render('event', {title: 'This Event'});
-})
-
-router.get('/events/event/edit', (req, res) => {
-  res.render('edit_event', ({title: 'Edit Event'}));
-})
-
-router.get('/events/add', (req, res) => {
-  res.render('create_event', ({title: "Create an Event"}));
+router.post('/events/add', (req, res) => {
+  let update = {};
+  update.title = req.body.title;
+  update.description = req.body.description;
+  update.date = req.body.date;
+  update.location = req.body.location;
+  update.photo = req.body.photo;
+  knex('events')
+  .insert(update)
+  .then(() => {
+    knex('events')
+    .then((events) => {
+      res.render('events', {title: 'Events', events: events});
+    });
+  });
 });
 
 module.exports = router;
